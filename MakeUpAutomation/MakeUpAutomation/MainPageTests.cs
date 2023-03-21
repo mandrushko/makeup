e33a06592400b510a67efffd78e0406ddc61bc7e
marinaandrushko <m.v.andrushko@gmail.com>
2023-03-21 12:42:23 +0200

    Add automation project MakeUp

---
+++ MakeUpAutomation/MakeUpAutomation/MainPageTests.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.Xml.Linq;
+
+namespace MakeUpAutomation
+{
+    [TestClass]
+    public class MainPageTests
+    {
+        private ChromeDriver driver;
+        private Actions action;
+
+        private By popUpClose = By.CssSelector("#popup__window > .popup-close");
+        private By buttonBrands = By.XPath("//a[contains(text(),'Бренди')]");
+        private By buttonL = By.XPath("//li[contains(.,'L')]");
+        private By itemLamelProfessionalLip = By.XPath("//a[contains(text(),'Lamel Professional')]");
+        private By hoverSliderLipLamelProfessional = By.XPath("//a[contains(text(),'Lamel Professional Lipliner')]");
+        private By buttonBuyItem = By.CssSelector(".active .button");
+        private By itemInBasket = By.CssSelector(".active .button");
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            action = new Actions(driver);
+            driver.Navigate().GoToUrl("https://makeup.com.ua/ua/");
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void OpenLinkBrands()
+        {
+
+            driver.FindElement(popUpClose).Click();
+
+            driver.FindElement(buttonBrands).Click();
+
+            driver.FindElement(buttonL).Click();
+
+            driver.FindElement(itemLamelProfessionalLip).Click();
+
+            action.MoveToElement(driver.FindElement(hoverSliderLipLamelProfessional)).Perform();
+
+            Assert.IsNotNull(buttonBuyItem);
+
+            driver.FindElement(buttonBuyItem).Click();
+
+            Assert.IsNotNull(itemLamelProfessionalLip);
+        }
+    }
+}
